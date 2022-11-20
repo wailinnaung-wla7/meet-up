a280f997806c14230998d0bbe74e40212b0dd10b
wailinnaung-wla7 <wailinnaung.wla7@gmail.com>
2022-11-20 22:22:49 +0630

    add meetup form

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,24 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-  return <div>New Meetup Page</div>;
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-d9bb7-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
